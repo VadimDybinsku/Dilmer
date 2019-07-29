ea724f58ef42cc6581adf2b96e7723b2cb31a119
Dilmer <gotohome83@gmail.com>
2019-07-29 19:02:10 +0300

    gitignore

--- Calendar_week.py
+++ Calendar_week.py
@@ -1,18 +1,3 @@
-from calendar import weekday
-date =input('Введите дату: ')
-day, month, year = map(int,date.split('.'))
-wd = weekday(year, month, day)
-if wd == 0:
-    print('Понедельник')
-if wd == 1:
-    print('Вторник')
-if wd == 2:
-    print('Среда')
-if wd == 3:
-    print('Четверг')
-if wd == 4:
-    print('Пятница')
-if wd == 5:
-    print('Суббота')
-if wd == 6:
-    print('Воскресенье')
+from calendar import weekday, day_name
+day, month, year = map(int,input('Введите дату: ').split('.'))
+print(*day_name[weekday(year, month, day)])
