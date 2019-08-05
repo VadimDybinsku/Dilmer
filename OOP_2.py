dca9791887211db667a0c85afbfa0e816e3cfd81
Dilmer <gotohome83@gmail.com>
2019-08-05 21:55:30 +0300

    gitignore

---
+++ OOP_2.py
@@ -0,0 +1,17 @@
+class Property(object):
+    def __init__(self, func):
+        self.func = func
+    def __get__(self, instance, owner):
+        return self.func(instance)
+
+class A(object):
+    def __init__(self, x,y):
+        self.x = x
+        self.y = y
+# Перезапись функции дискриптором
+    @Property
+    def z(self):
+        return self.x + self.y
+
+a = A(1,2)
+print(a.z)
