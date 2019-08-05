d1614647af35648bce877d63739c191d21c5e414
Dilmer <gotohome83@gmail.com>
2019-08-05 21:45:46 +0300

    gitignore

--- OOP_1.py
+++ OOP_1.py
@@ -3,12 +3,25 @@ def f(self):
 
 class Descriptor(object):
     def __get__(self, instance, owner):
-        def wrapper():
-            return f(instance)
+        def wrapper(*args):
+            return f(instance, *args)
         return wrapper
+
 class A(object):
-    f = Descriptor
+    def f(self):
+        pass
 
 a = A()
-print(a)
-a.f()
+print(a.f)
+print('f' in a.__dict__)
+value = None
+for base in A.__mro__:
+    print(f"{base.__name__}: {'f'in base.__dict__}")
+    if 'f' in base.__dict__:
+        value = base.__dict__['f']
+        break
+getter = getattr(value,'__get__', None)
+print(getter)
+
+print(getter(a, A))
+print(a.f)
